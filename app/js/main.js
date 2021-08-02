453aa0c88ae39c37d573442b9d0cb201de111805
zur1k <zazamuradov@gmail.com>
2021-08-02 14:07:49 +0300

    add slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,17 @@
 $(function () {
+	// right side menu - чтобы добавлялся и убирался класс (close)
 	$('.header__btn').on('click', function () {
 		$('.rightside-menu').removeClass('rightside-menu--close')
 	})
 	$('.rightside-menu__close').on('click', function () {
 		$('.rightside-menu').addClass('rightside-menu--close')
 	})
+	//slick-slider 
+	$('.top__slider').slick({
+		dots: true,
+		arrows: false,
+		fade: true,
+		autoplay: true
+	})
+
 })
