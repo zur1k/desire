7a3639135d15e0247e929e2aa570787bccf324f2
zur1k <zazamuradov@gmail.com>
2021-08-22 16:22:28 +0300

    responsive about.page contact.page

--- app/js/main.js
+++ app/js/main.js
@@ -28,7 +28,55 @@ $(function () {
 		slidesToScroll: 10,
 		dots: true,
 		arrows: false,
+		responsive: [
+			{
+				breakpoint: 1700,
+				settings: {
+					slidesToShow: 8,
+					slidesToScroll: 8,
 
+				}
+			},
+			{
+				breakpoint: 1511,
+				settings: {
+					slidesToShow: 6,
+					slidesToScroll: 6,
+				}
+			},
+			{
+				breakpoint: 1201,
+				settings: {
+					slidesToShow: 4,
+					slidesToScroll: 4,
+				}
+			},
+			{
+				breakpoint: 841,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 3,
+				}
+			},
+			{
+				breakpoint: 551,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2,
+				}
+			},
+			{
+
+				breakpoint: 376,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+				}
+
+			}
+
+
+		]
 	})
 	$('.article-slider__box').slick({
 		prevArrow: '<button type="button" class="article-slider__arrow article-slider__arrowleft"><img src="images/arrow-slide-left.svg" alt="arrow-left"></button>Previous</button>',
